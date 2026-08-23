f0b42f15960d1e7a875a1335249861f2a0bd3e45
greenastrategy-hash <greenastrategy@gmail.com>
2026-08-23 20:27:10 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// นำ Web App URL ที่ได้จากขั้นตอน Deploy ของ Google Apps Script มาวางที่นี่
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbx8AQznXGrx9BGbCln6r3m9dsU7yF2w6vSCt8vMgoi7fHvR7icflL7GOCA-U8X5HmIG/exec";
